f4ea66becbf7819e03d01337ea9af8b1cbc1cbe9
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-06-18 19:05:25 -0400

    Pokemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Gallery from './Components /Gallery'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ function App() { const [data, setData] = useState([]) 
+  const [apiUrl, setApiUrl] = useState('https://pokeapi.co/api/v2/pokemon') 
+  const [nextUrl, setNextUrl] = useState('') 
+  const [previousUrl, setPreviousUrl] = useState('') 
+
+
+ useEffect(() => { 
+  const fetchData = async () => { 
+   const response = await fetch(apiUrl) 
+   const data = await response.json()
+
+   setData(data.results) 
+   setNextUrl(data.next)
+   setPreviousUrl(data.previous) 
+  } 
+    
+    fetchData() 
+  }, [apiUrl]) 
+
+  const fetchPreviousPage = () => {
+    if (!previousUrl) return 
+      setApiUrl(previousUrl) 
+    } 
+
+  const fetchNextPage = () => { 
+    if (!nextUrl) return 
+      setApiUrl(nextUrl) 
+    } 
+        
+    return (
+      <div> 
+        <h1>Pokemon!</h1> 
+        <div style={{'display': 'inline-block'}} >
+          <button onClick={fetchPreviousPage}>Previous</button>
+          <button onClick={fetchNextPage}>Next</button> 
+        </div>
+        <Gallery data={data}/>
+     </div>
   );
 }
 
---
+++ src/Components /Gallery.js
@@ -0,0 +1,14 @@
+function Gallery ({ data }){
+    const display =  data.map(pokemon => {
+       return(
+           <li key={pokemon.name}>{pokemon.name}</li>
+       )
+     })
+     return(
+        <div>
+           {display}
+        </div>
+     )
+    }
+   
+    export default Gallery
